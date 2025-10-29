8fffb85e7263892ac01b87aaff40c9cf52ec0d92
배상도 <sangdobae@deep-medi.com>
2025-10-29 14:16:29 +0900

    Time: 3 ms (73.5%), Space: 18.1 MB (20.85%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        len_cost = len(cost)
+        dp = [0] * len_cost
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len_cost):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+
+        return min(dp[-1], dp[-2])
